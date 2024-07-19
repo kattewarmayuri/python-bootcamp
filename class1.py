74861b25afe60ca5b532b08990370045ed0b3a40
kattewarmayuri <chakuli.kathewar@gmail.com>
2024-07-19 05:08:51 -0500

    day-1

---
+++ class1.py
@@ -0,0 +1,10 @@
+name=input()
+#s1=int(input())
+#s2=int(input())
+#s3=int(input())
+#print(f"hello your marks are {s1+s2+s3} avg is{s1+s2+s3/3}")
+age=25
+if(age>18):
+    print( f"your age is {age}  your eligible for voiting") 
+elif(age<18):
+    print(f"your age is {age}  your not eligible for voiting")   
